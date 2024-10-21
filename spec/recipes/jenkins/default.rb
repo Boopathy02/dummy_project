17a79cf172736f5db11d483a7bc4c8e0ff5f94bd
Boopathy02 <nboopathy02@gmail.com>
2024-10-21 16:14:02 +0530

    inital commit

---
+++ spec/recipes/jenkins/default.rb
@@ -0,0 +1,28 @@
+# Setup jenkins configuration files
+
+group "jenkins" do
+  gid 123
+end
+
+user "jenkins" do
+  uid 1234
+  gid 123
+  create_home false
+end
+
+include_recipe "./jobs"
+include_recipe "./nodes"
+include_recipe "./plugins"
+include_recipe "./secrets"
+include_recipe "./users"
+
+%w(
+  hudson.model.UpdateCenter.xml
+  saml-jenkins-keystore.jks
+).each do |file|
+  remote_file "#{node[:jenkins_home]}/#{file}" do
+    mode  "644"
+    owner "jenkins"
+    group "jenkins"
+  end
+end
